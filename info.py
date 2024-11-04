9aefefac96f853967600db9167cbcaf169c7a085
HeartTheft <110565010+HeartTheft@users.noreply.github.com>
2024-11-04 18:27:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Movieprovidergr
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002489528366').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Ajifilterbot:1055221@ajifilterbot.pjrma.mongodb.net/?retryWrites=true&w=majority&appName=AjiFilterbot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AutoFilter:<db_password>@heartfilter.rrt16.mongodb.net/?retryWrites=true&w=majority&appName=HeartFilter")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Ajifilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
