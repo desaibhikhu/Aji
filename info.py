b40a259b6c10162db041048fcf21cc0234f3b9d5
desaibhikhu <85123393+desaibhikhu@users.noreply.github.com>
2025-02-07 21:51:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '507206621').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Heart_thieft") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-100232719014'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002327190146'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/boatrixmovierequest')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002354322033').split()]
 #---------------------------------------------------------------
